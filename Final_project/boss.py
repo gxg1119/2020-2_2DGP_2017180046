80207d31696ee5c25b6730b2af93706c94dbd865
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2020-11-19 00:30:33 +0900

    boss 리소스 추가, boss.py 생성

---
+++ Final_project/boss.py
@@ -0,0 +1,19 @@
+from pico2d import *
+import gfw
+from gobj import *
+
+class Boss:
+	def __init__(self):
+		pass
+
+	def draw(self):
+		pass
+
+	def update(self):
+		pass
+
+	def remove(self):
+		pass
+
+	def get_bb(self):
+		pass
