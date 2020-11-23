f74364684aab4cd1bb2f0cc47506c251e000220d
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2020-11-24 03:54:13 +0900

    아이템 듀얼샷 초기 구현 (30%)
    경우를 나눠서 0일때는 평범한 샷
    1일 때는 듀얼샷 3초 정도 나오도록 구현 중

--- Final_project/bullet.py
+++ Final_project/bullet.py
@@ -11,16 +11,25 @@ class LaserBullet:
         self.dy = speed
         self.image = gfw.image.load(RES_DIR + '/bullet_01.png')
         self.power = 50
+        self.Shoot_state = 1
+        self.Dualshoot_time = 3
 
     def draw(self):
-        self.image.draw(self.x, self.y + 75)
+        if self.Shoot_state == 1 and self.Dualshoot_time > 0:
+            self.image.draw(self.x-50, self.y + 75)
+            self.image.draw(self.x+50, self.y + 75)
+        else:
+            self.image.draw(self.x, self.y + 75)
 
     def update(self):
         self.y += self.dy * gfw.delta_time
-
+        self.Dualshoot_time -= gfw.delta_time
+        print(self.Dualshoot_time)
         if self.y > get_canvas_height() + LaserBullet.SIZE:
             self.remove()
 
+        if self.Shoot_state == 1:
+            pass
     def remove(self):
         gfw.world.remove(self)
 
