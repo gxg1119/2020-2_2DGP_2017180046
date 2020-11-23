f45ea73600f030bc4046bd194b7d04f5392e4e63
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2020-11-24 01:10:19 +0900

    money 내용 수정

--- Final_project/money.py
+++ Final_project/money.py
@@ -12,6 +12,8 @@ class Money:
 		self.dx, self.dy = dx*0.5, -random.uniform(2,3)
 		self.image = gfw.image.load(RES_DIR + '/money.png')
 
+		self.side = random.randint(1, 2)
+
 	def update(self):
 		x, y = self.x, self.y
 		dx, dy = self.dx, self.dy
@@ -23,6 +25,9 @@ class Money:
 
 		self.x, self.y = x, y
 
+		if self.side == 1: self.dx = -1
+		else: self.dx = 1
+
 	def draw(self):
 		self.image.draw(self.x, self.y)
 
@@ -30,11 +35,7 @@ class Money:
 		gfw.world.remove(self)
 
 	def generate(self):
-		self.side = random.randint(1, 2)
-		if self.side == 1: self.dx = -1
-		else: self.dx = 1
-
-		money=Money(self.x, self.y, self.dx, self.dy)
+		money = Money(self.x, self.y, self.dx, self.dy)
 		gfw.world.add(gfw.layer.money, money)
 
 	def get_bb(self):
