236b96aa6da6a2f71443fbd86a566a188a138ee4
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2020-12-01 07:00:51 +0900

    character select state에 캐릭터 일러스트 추가

--- Final_project/character_select_state.py
+++ Final_project/character_select_state.py
@@ -4,13 +4,23 @@ from gobj import *
 import Play_state
 
 def enter():
-	pass
-
+	global boximage, chimage, chsimage, x, y, dy
+	boximage = gfw.image.load_image(RES_DIR + '/box_01.png')
+	chimage = gfw.image.load_image(RES_DIR + '/character_01.png')
+	chsimage = gfw.image.load_image(RES_DIR + '/select_character_01.png')
+	x, y = get_canvas_width() // 2, get_canvas_height() // 2
+	dy = 0.5
 def update():
-    pass
+    global y, dy
+    print(y)
+    y += dy
+    if y < 490 or y > 510: dy *= -1
 
 def draw():
-	pass
+	global x, y
+	chimage.draw(x, y, 700, 700)
+	boximage.draw(get_canvas_width() // 2, 150,700,150)
+	chsimage.draw(get_canvas_width() // 2, 150)
 
 def handle_event(e):
     if e.type == SDL_QUIT:
