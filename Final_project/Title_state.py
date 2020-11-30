1696ca5492d768a6051ca5eb03dc7ec212c60d11
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2020-12-01 04:28:51 +0900

    Title_state 배경음악 추가, 버튼 클릭 사운드 추가

--- Final_project/Title_state.py
+++ Final_project/Title_state.py
@@ -9,6 +9,11 @@ def enter():
     game_start_box = load_image(RES_DIR + '/Gamestart.png')
     game_end_box = load_image(RES_DIR + '/Gameend.png')
 
+    global music_bg, botton_sd
+    music_bg = load_music(RES_DIR +'/Title_bg_music.mp3')
+    botton_sd = load_wav(RES_DIR +'/button.wav')
+    music_bg.repeat_play()
+
 def update():
     pass
 
@@ -24,8 +29,10 @@ def handle_event(e):
         gfw.quit()
     elif (e.type, e.button) == (SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT):
         if(e.x >= 50 and e.x<=350 and e.y <= 925 and e.y >= 775):
+            botton_sd.play()
             gfw.push(Play_state)
         elif(e.x >= 400 and e.x<=700 and e.y <= 925 and e.y >= 775):
+            botton_sd.play()
             gfw.quit()
             
 def exit():
