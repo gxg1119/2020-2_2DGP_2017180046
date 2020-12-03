ec811059f292281583edf030a350c22b26862180
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2020-12-03 22:36:36 +0900

    캐릭터 - 몬스터 충돌시 이펙트 추가

--- Final_project/player.py
+++ Final_project/player.py
@@ -2,7 +2,7 @@ import random
 from pico2d import *
 import gfw
 from gobj import *
-from bullet import *
+from bullet import LaserBullet
 
 MAX_LIFE = 3
 
@@ -26,7 +26,6 @@ class Player:
         self.speed = 750
         self.p1image = gfw.image.load(RES_DIR + '/Player_01.png')
         self.p2image = gfw.image.load(RES_DIR + '/Player_02.png')
-        #half = self.image.w
         self.minx = 75
         self.maxx = 675
         self.fidx = 0
@@ -39,8 +38,11 @@ class Player:
         self.hp_2 = gfw.image.load(RES_DIR + '/hp_heart_02.png')
 
         self.damage_time = 0
+        self.dfidx = 0
+        self.damage_ox = False
         self.damage = gfw.image.load(RES_DIR + '/damage.png')
 
+        self.dualshoot_cnt = 0
         self.powershoot_cnt = 9
         self.powershoot_wav = load_wav(RES_DIR +'/power_shot.wav')
 
@@ -67,7 +69,11 @@ class Player:
             self.hp = self.hp_1 if i < self.life else self.hp_2
             self.hp.draw(self.hx, self.hy, 40, 40)
             self.hx -= self.hp.w + 10
-        self.damage.draw(self.x+40, self.y+50)
+        if self.damage_ox == True:
+            if self.damage_time <= 3:
+                self.damage.clip_draw(self.dfidx * 64, 0, 64, 64, self.x+40, self.y+50)
+            else : self.damage_ox = False
+        else : self.damage_time = 0
 
     def update(self):
         self.x += self.dx * self.speed * gfw.delta_time
@@ -82,8 +88,9 @@ class Player:
 
         if self.laser_time >= Player.LASER_INTERVAL:
             self.fire()
-        if self.damage_time >= Player.LASER_INTERVAL :
-            print("ddfa")
+        if self.damage_ox == True:
+            self.damage_time += gfw.delta_time
+            self.dfidx = int(self.damage_time * 2) % 2
             
     def handle_event(self, e):
         pair = (e.type, e.key)
@@ -95,6 +102,14 @@ class Player:
                 LaserBullet.Shoot_state = 2
                 LaserBullet.Power = 500
                 self.powershoot_wav.play()
+        if pair == (SDL_KEYDOWN, SDLK_LSHIFT):
+            print(self.dualshoot_cnt)
+            if self.dualshoot_cnt > 0 :
+                if LaserBullet.powershoot_time == 0 and self.dualshoot_cnt > 0: 
+                    LaserBullet.Shoot_state = 1
+                    LaserBullet.Dualshoot_time = 20
+
+                self.dualshoot_cnt -= 1
 
     def get_bb(self):
         hw = 30
